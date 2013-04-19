d8b30f7ec14a50b9933e520c061415da4f31cf1c
Tetsushi Ideura <tetsuta.shotaro@gmail.com>
2013-04-19 20:30:21 +0900

    first comit

---
+++ HelloGit/src/jp/idebow/hellogit/HelloGit.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package jp.idebow.hellogit;
+
+/**
+ * @author ideura
+ *
+ */
+public class HelloGit {
+
+	/**
+	 *
+	 */
+	public HelloGit() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+	}
+
+}
